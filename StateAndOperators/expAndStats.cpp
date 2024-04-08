e5b6723ef911c6ac779dae9adb42bcfb2dac820f
Francisco Procopio <francisconeto.cg@gmail.com>
2024-04-08 06:55:07 -0300

    statements and operators

---
+++ StateAndOperators/expAndStats.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 {10}; // Initialization
+    int num2 {20}; // Initialization
+
+    num1 = 100; // Assignement
+
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+    
+    return 0;
+}
